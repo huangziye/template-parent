b2a9f172f8fec1bed23044030d893bdf7e31fb42
ziye_huang <ziye_huang@163.com>
2019-06-11 14:42:01 +0800

    增加热部署及执行脚本。

---
+++ template-domain/src/main/java/com/hzy/bean/User.java
@@ -0,0 +1,16 @@
+package com.hzy.bean;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author: ziye_huang
+ * @date: 2019/6/10
+ */
+@Getter
+@Setter
+public class User {
+    private String username;
+    private String password;
+}
